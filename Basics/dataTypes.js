575aaa6489724820acee10b8ecbdd53aef07c166
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-06-20 12:50:59 +0000

    DataTypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,18 @@
+let var1 = "prabhat"
+let var2 = 4
+
+/*
+number = 2 to power 53
+bigint = extremely large number
+string = ""
+boolean = true/false
+null = standalone value
+undefined
+symbols = uniqueness
+
+objects
+*/
+
+console.log(typeof var1);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
