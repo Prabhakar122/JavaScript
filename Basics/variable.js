1b38365ecaf8cd13c79511e14161a567a69e74d9
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-06-20 12:49:18 +0000

    variables

---
+++ Basics/variable.js
@@ -0,0 +1,13 @@
+const employeeId = 12453
+let name = "prabhat"
+var email = 'prabhat@gmail.com'
+address = "Rajapur"
+let age;
+
+// console.log(employeeId);
+
+console.table([employeeId,name,email,address,age]);
+
+/*
+prefer not to use var because of issue of functional scope
+*/
