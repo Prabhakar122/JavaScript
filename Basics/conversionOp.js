1178388801df928daf734f1afd859130a79c4c8c
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-06-21 16:24:00 +0000

    DataType Conversion

---
+++ Basics/conversionOp.js
@@ -0,0 +1,33 @@
+let score;
+console.log(typeof(score));
+
+// let Value = Number(score)
+// let Value = Boolean(score)
+let Value = String(score)
+console.log(typeof Value);
+console.log(Value);
+
+/*
+Number(variable) = to number
+
+"" = 0
+"3" = 3
+"3abc" = NaN
+null = 0
+undefinded = NaN
+
+
+Boolean(variable) = to true/false
+
+1 = true
+0 = false
+"" = false
+"0" = true
+"3abc" = true
+
+String(variable) = to string
+
+null = null
+undefined = undefined
+33 = "33"
+*/
