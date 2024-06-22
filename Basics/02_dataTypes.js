b9eadf246b2c22ebd9407beb82d1927e1edff28e
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-06-22 09:31:45 +0000

    DataTypes

--- Basics/02_dataTypes.js
+++ Basics/02_dataTypes.js
@@ -13,6 +13,10 @@ symbols = uniqueness
 objects
 */
 
+let id = Symbol("123")
+let anotherId = Symbol("123")
+console.log(id == anotherId); //same input but different symbols
+
 console.log(typeof var1);
 console.log(typeof null); //object
 console.log(typeof undefined); //undefined
