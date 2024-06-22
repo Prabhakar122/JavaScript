709d4555c75f44e0552e8cc943eecf7f96832235
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-06-22 08:24:19 +0000

    Conversion of DataTypes

---
+++ Basics/03_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 == 2);
+// console.log(2 <= 2);
+// console.log(2 >= 2);
+// console.log(2 < 2);
+// console.log(2 > 2);
+// console.log(2 != 2);
+
+// console.log("2" == 2);
+// console.log("02" == 2);
+
+// console.log(null == 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// strict check = checks both values and datatypes without conversion
+console.log("2" > 2);
+/*
+while comparing datatypes must be same
+*/
