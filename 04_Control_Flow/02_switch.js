6e43d5aa2493b8577d2521a96095391eef00b27f
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-07-02 17:20:18 +0000

    switch case

---
+++ 04_Control_Flow/02_switch.js
@@ -0,0 +1,20 @@
+/********Switch statement********* */
+
+const day = "Wed"
+switch (day) {
+    case "Sun":
+        console.log("Sunday");
+        break;
+    case "Mon":
+        console.log("Monday");
+        break;
+    case "Tue":
+        console.log("Tuesday");
+        break;
+    case "Wed":
+        console.log("Wednesday");
+        break;
+        default:
+        console.log("This is the default condition");
+        break;
+}
