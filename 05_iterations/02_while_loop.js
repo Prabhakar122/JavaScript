1697213f7e1e84427c0c4812989bd0c43c744042
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-07-31 16:32:26 +0000

    while loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,20 @@
+/********While loop******* */
+
+let index = 1;
+while (index <= 5) {
+    // console.log(index);
+    index++;
+}
+
+let myarray = [ "Batman", "Superman", "Aquaman"]
+let Avariable = 0;
+while (Avariable < myarray.length) {
+    // console.log(`${myarray[Avariable]}`);
+    Avariable++;
+}
+
+let marks = 10;
+ do {
+    console.log(` marks = ${marks}`);
+    marks = marks + 10;
+ } while (marks < 100);
