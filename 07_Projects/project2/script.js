3f72ccf60b23b20bbb1abbb5e0c4a4b548adb2a6
Prabhakar122 <125270069+Prabhakar122@users.noreply.github.com>
2024-08-05 16:56:00 +0000

    bmi calculator

--- 07_Projects/project2/script.js
+++ 07_Projects/project2/script.js
@@ -6,21 +6,24 @@ submittedForm.addEventListener('submit', function(event){
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#result')
-    if (height == '' || height < 0 || isNaN(height)) {
+    if (height == '' || height <= 0 || isNaN(height)) {
         result.innerHTML = `Enter valid height ${height}`
         console.log(height);
-    }else if (weight == '' || weight < 0 || isNaN(weight)) {
+    }else if (weight == '' || weight <= 0 || isNaN(weight)) {
         result.innerHTML = `Enter valid weight ${weight}`
         console.log(weight);
     }else{
         const val = (weight/((height*height)/10000)).toFixed(2);    
         if (val < 18.5) {
-            const msg = document.createTextNode(`Under weight : ${val}`)
-            result.appendChild(msg)
-        } else if {
-            const msg = document.createTextNode(`Under weight : ${val}`)
-            result.appendChild(msg)
-            
+            result.innerHTML = `Underweight : ${val}`
+        } else if( val >= 18.5 && val < 25){
+            result.innerHTML = `Healthy weight : ${val}`            
+        } else if( val >= 25 && val < 30){
+            result.innerHTML = `Overweight : ${val}`
+        } else if( val >= 30 && val < 40){
+            result.innerHTML = `Obese : ${val}`
+        }else{
+            result.innerHTML = `Severly obese : ${val}`
         }
     }
 })
